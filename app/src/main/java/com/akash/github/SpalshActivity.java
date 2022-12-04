ff9cecb4151cfd7edbbc2b0d9f27da59452e3c47
Akash <afjalAn1512>
2022-12-04 16:06:32 +0600

    hello coder

---
+++ app/src/main/java/com/akash/github/SpalshActivity.java
@@ -0,0 +1,14 @@
+package com.akash.github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpalshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+    }
+}
